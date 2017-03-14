f0a9f838882fb6e4999356dde7ef2dfb99ed7311
Scott Heng <scottheng3@gmail.com>
2017-03-14 02:41:14 -0700

    create store

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as sessionAPIUtil from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+    const store = configureStore();
     window.signup = sessionAPIUtil.signup;
     window.login = sessionAPIUtil.login;
     window.logout = sessionAPIUtil.logout;
+    window.store = store;
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to inQuizitive!</h1>, root);
 });
