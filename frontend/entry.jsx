7e380d49dbc35ceffe0fdad391ff9994b5deb2de
Scott Heng <scottheng3@gmail.com>
2017-03-14 02:08:27 -0700

    create api util functions to make ajax requests

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as sessionAPIUtil from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
+    window.signup = sessionAPIUtil.signup;
+    window.login = sessionAPIUtil.login;
+    window.logout = sessionAPIUtil.logout;
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to inQuizitive!</h1>, root);
 });
