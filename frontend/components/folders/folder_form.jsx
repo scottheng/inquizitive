7d9e6c7041c27dce58885d99062ec2023f96312e
Scott Heng <scottheng3@gmail.com>
2017-03-22 06:32:26 -0700

    create folder form

---
+++ frontend/components/folders/folder_form.jsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { Link, hashHistory } from 'react-router';
+
+class FolderForm extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { name: "", description: "" };
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	update(field) {
+		return (e) => this.setState({[field]: e.currentTarget.value});
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		const folder = this.state;
+		this.props.processForm(folder)
+		.then(action => hashHistory.push(`/`));
+	}
+
+	renderErrors() {
+		return (
+			<ul className="session-errors">
+				{this.props.errors.map((error, idx) => (
+					<li key={`erro-${idx}`}>
+						{error}
+					</li>
+				))}
+			</ul>
+		);
+	}
+
+	render() {
+
+		return (
+			<div className="folder-form">
+				<form onSubmit={this.handleSubmit} className="folder-form-box">
+					<header className='form-title'>
+						<h1>{this.props.formType} folder</h1>
+					</header>
+					<br/>
+					{this.renderErrors()}
+					<div className="folder-form-inputs">
+						<br/>
+						<div className='form-input'>
+							<input type="text"
+									value={this.state.name}
+									placeholder="Enter a title"
+									onChange={this.update("name")}
+									className="folder-input" />
+							<label className="input-label">
+								NAME 
+							</label>
+						</div>
+						<div className='form-input'>
+							<input type="text"
+									value={this.state.description}
+									placeholder="Enter a description (optional)"
+									onChange={this.update("description")}
+									className="folder-input" />
+							<label className="input-label">
+								DESCRIPTION
+							</label>
+						</div>
+						<br />
+						<input type="submit" value={this.props.formType} />
+					</div>
+				</form>
+			</div>
+		);
+	}
+}
+
+export default FolderForm;
---
+++ frontend/components/folders/folder_form_container.jsx
@@ -0,0 +1,21 @@
+import { connect } from 'react-redux';
+import FolderForm from './folder_form';
+import { createFolder, updateFolder } from '../../actions/folder_actions';
+
+const mapStateToProps = (state, ownProps) => ({
+	formType: ownProps.formType,
+	errors: state.folder.errors
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+	const processForm = (ownProps.formType === 'new') ? createFolder : updateFolder;
+	return {
+		processForm: (folder) => dispatch(processForm(folder))
+	};
+};
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(FolderForm);
+
