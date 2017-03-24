ec4e9dd357844330eb545abd5294e477a3234a58
Scott Heng <scottheng3@gmail.com>
2017-03-23 23:49:32 -0700

    toggle between flashcards

--- frontend/components/studysets/flashcards.jsx
+++ frontend/components/studysets/flashcards.jsx
@@ -3,21 +3,111 @@ import React from 'react';
 class Flashcards extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {cards: this.props.studySet.cards, cardIndex: 0};
+		this.prevCard = this.prevCard.bind(this);
+		this.nextCard = this.nextCard.bind(this);
 	}
 
+	componentDidMount() {
+		this.props.fetchStudySet(this.props.params.studySetId);
+	}
+
+	componentWillReceiveProps(nextProps) {
+		this.setState({cards: nextProps.studySet.cards});
+	
+	}
+
+	prevCard(e) {
+		e.preventDefault();
+		if (this.state.cardIndex - 1 >= 0) {
+			this.setState({cardIndex: this.state.cardIndex - 1});
+		}
+	}
+
+	nextCard(e) {
+		e.preventDefault();
+		if (this.state.cards[this.state.cardIndex + 1]) {
+			this.setState({cardIndex: this.state.cardIndex + 1});
+		}
+	}
+
+
 	render() {
+
+		const front = () => {
+			if (this.state.cards[this.state.cardIndex]) {
+				return (
+					<h1>{this.state.cards[this.state.cardIndex].term}</h1>
+				);
+			}
+		};
+
+		const back = () => {
+			if (this.state.cards[this.state.cardIndex]) {
+				return (
+					<h1>{this.state.cards[this.state.cardIndex].definition}</h1>
+				);
+			}
+		};
+
+		const prevButton = () => {
+			if (this.state.cardIndex - 1 < 0) {
+				return (
+					<div onClick={this.prevCard}
+							className="inactive">
+						<i className="fa fa-chevron-circle-left" aria-hidden="true"></i>
+					</div>
+				);
+			}
+			else {
+				return (
+					<button onClick={this.prevCard}
+							className="active">
+						<i className="fa fa-chevron-circle-left" aria-hidden="true"></i>
+					</button>
+				);				
+			}
+		};
+
+		const nextButton = () => {
+			if (this.state.cardIndex + 1 >= this.state.cards.length) {
+				return (
+					<div onClick={this.nextCard}
+							className="inactive">
+						<i className="fa fa-chevron-circle-right" aria-hidden="true"></i>
+					</div>
+				);
+			}
+			else {
+				return (
+					<button onClick={this.nextCard}
+							className="active">
+						<i className="fa fa-chevron-circle-right" aria-hidden="true"></i>
+					</button>
+				);
+			}
+		};
+
 		return (
 			<div className="flashcards-page">
 				<div className="flip-container" >
 					<div className="flipper">
 						<div className="front">
-							<h1>Hello</h1>
+							{front()}
 						</div>
 						<div className="back">
-							<h1>Goodbye</h1>
+							{back()}
 						</div>
 					</div>
 				</div>
+				<ul className="prev-next-buttons">
+					<li>
+						{prevButton()}
+					</li>
+					<li>
+						{nextButton()}
+					</li>
+				</ul>
 			</div>
 
 		);
--- frontend/components/studysets/flashcards_container.jsx
+++ frontend/components/studysets/flashcards_container.jsx
@@ -1,13 +1,14 @@
 import { connect } from 'react-redux';
 import Flashcards from './flashcards';
+import { fetchStudySet } from '../../actions/study_set_actions';
 
 const mapStateToProps = state => ({
 	currentUser: state.session.currentUser,
-	
+	studySet: state.studySet
 });
 
 const mapDispatchToProps = dispatch => ({
-
+	fetchStudySet: (id) => dispatch(fetchStudySet(id))
 });
 
 export default connect(
